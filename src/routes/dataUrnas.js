435d3e8ce355c8b5a5683e4c975d70a3d216f7f0
miguelkenny <miguelcastrokenny@gmail.com>
2025-05-11 22:35:05 -0300

    Aramado de Catalogo

--- src/routes/dataUrnas.js
+++ src/routes/dataUrnas.js
@@ -47,6 +47,46 @@ export default [
     tipo: "std"
   },
   {
+    slug: "urna-simil-piedra",
+    nombre: "Urna Simil Piedra",
+    descripcion: "Un Refugio de Eternidad",
+    descripcionLarga: "Esta urna, con su base color arena, guarda el susurro de la tierra cálida que ha sostenido cada paso de quien partió. Sobre ella, el turquesa se eleva como cielo abierto, recordando que el alma no se encierra, sino que se libera. Las gaviotas, que vuelan en armonía entre el horizonte y la brisa, son mensajeras de la trascendencia. En su vuelo sereno sobre la arena, nos hablan de despedidas suaves y retornos invisibles, de un viaje que no es final, sino transformación. Este modelo es un símbolo del vínculo eterno entre lo que fuimos y lo que aún somos en la memoria de quienes nos aman.La arena representa lo vivido, lo tangible, mientras que el cielo evoca lo espiritual, lo que se eleva.Y entre ambos, la promesa de un descanso en libertad, en paz y belleza. Una casita para el alma, donde el mar interior se encuentra con el cielo eterno.",
+    imagen: ["/images/2500-simil-piedra.png"],
+    capacidad: "2.5 Lts.",
+    gramos: "1250 - 1500",
+    tipo: "prm"
+  },
+  {
+    slug: "urna-1500-piedra",
+    nombre: "Urna Simil Piedra",
+    descripcion: "Diseño sobrio que representa estabilidad y fortaleza.",
+    descripcionLarga: "Sólida, simple y noble. Piedra simboliza estabilidad, presencia y permanencia. Su diseño sobrio en color liso rinde homenaje a quienes fueron pilares en la vida de sus seres queridos.",
+    imagen: ["/images/1500-simil-piedra.png"],
+    capacidad: 1.500,
+    gramos: "700 - 800",
+    tipo: "prm"
+  },
+  {
+    slug: "urna-1750-pluma",
+    nombre: "Urna Pluma 1.750",
+    descripcion: "Una urna azul serena con una pluma plateada, símbolo de liviandad, trascendencia y paz.",
+    descripcionLarga: "Como una caricia del viento, esta urna azul transmite serenidad y profundidad espiritual. La pluma plateada grabada en su superficie representa la ligereza del alma en su viaje, el desprendimiento suave y la conexión con lo sutil. Inspirada en la idea de que el alma no pesa, sino que flota hacia lo eterno, esta urna es ideal para quienes vivieron con sensibilidad, sabiduría y una presencia que aún se percibe, aunque ya no se vea. El azul evoca la inmensidad del cielo y la calma de las aguas profundas, mientras que la pluma simboliza guía, memoria y libertad.",
+    imagen: ["/images/1750-pluma.png"],
+    capacidad: 1.750,
+    gramos: "850 - 950",
+    tipo: "prm"
+  },
+  {
+    slug: "urna-2500-pluma",
+    nombre: "Urna Pluma 2.500",
+    descripcion: "Una urna azul serena con una pluma plateada, símbolo de liviandad, trascendencia y paz.",
+    descripcionLarga: "Como una caricia del viento, esta urna azul transmite serenidad y profundidad espiritual. La pluma plateada grabada en su superficie representa la ligereza del alma en su viaje, el desprendimiento suave y la conexión con lo sutil. Inspirada en la idea de que el alma no pesa, sino que flota hacia lo eterno, esta urna es ideal para quienes vivieron con sensibilidad, sabiduría y una presencia que aún se percibe, aunque ya no se vea. El azul evoca la inmensidad del cielo y la calma de las aguas profundas, mientras que la pluma simboliza guía, memoria y libertad.",
+    imagen: ["/images/2500-pluma.png"],
+    capacidad: "2.5 Lts.",
+    gramos: "1250 - 1500",
+    tipo: "prm"
+  },
+  {
     slug: "urna-horizonte",
     nombre: "Urna Horizonte",
     descripcion: "Un Refugio de Eternidad",
@@ -55,5 +95,38 @@ export default [
     capacidad: "2.5 Lts.",
     gramos: "1250 - 1500",
     tipo: "prm"
-  }
+  },
+  {
+    slug: "urna-otono",
+    nombre: "Urna Otoño",
+    descripcion: "Una urna color caramelo con hojas bronce, símbolo del ciclo natural, la belleza del cambio y la calidez del recuerdo.",
+    descripcionLarga: "La urna Otoño celebra la sabiduría del tiempo y el valor de cada etapa vivida. Su tono caramelo evoca la tierra cálida, el hogar y el abrazo del pasado. Las hojas bronce, cuidadosamente dispuestas, representan la transformación, el soltar con gratitud y el legado que permanece. Como los árboles que dejan ir sus hojas para renovarse, esta urna honra a quienes supieron cerrar ciclos con plenitud, dejando huellas nobles y profundas. Es una casita para almas que vivieron intensamente, con la templanza del otoño y el corazón lleno de historias.",
+    imagen: ["/images/2500-otono.png"],
+    capacidad: "2.5 Lts.",
+    gramos: "1250 - 1500",
+    tipo: "prm"
+  },
+  {
+    slug: "urna-colibri",
+    nombre: "Urna Colibri 1.750",
+    descripcion: "Una urna delicadamente decorada con un colibrí pintado a mano, símbolo de alegría, movimiento y conexión con lo sagrado.",
+    descripcionLarga: "El colibrí, con su vuelo ágil y etéreo, ha sido símbolo de ligereza, renovación y la trascendencia del alma. En muchas culturas, se le atribuye la misión de unir el mundo terrenal con el espiritual, llevando mensajes de amor y esperanza más allá del tiempo. En una urna cerámica, su presencia se convierte en un emblema de eternidad: un recordatorio de que aquellos que amamos no se desvanecen, sino que permanecen en la brisa, en el canto de la naturaleza y en cada instante de belleza que nos rodea. Como el colibrí que desafía el viento con su ímpetu, el alma que habita en su refugio no se apaga, sino que sigue vibrando en el recuerdo y el amor de quienes la honran. Esta urna es más que un receptáculo; es un santuario de memoria, un homenaje delicado a la vida que continúa su vuelo, sutil y eterno, en los corazones de quienes siguen su estela.",
+    imagenes: [
+      "/images/1750-colibri.png",
+      "/images/1750-colibri2.png"
+    ],
+    capacidad: 1.750,
+    gramos: "850 - 950",
+    tipo: "prm"
+  },
+  {
+    slug: "urna-luz",
+    nombre: "Urna Luz 1.700",
+    descripcion: "Una urna delicadamente decorada con un colibrí pintado a mano, símbolo de alegría, movimiento y conexión con lo sagrado.",
+    descripcionLarga: "El colibrí, con su vuelo ágil y etéreo, ha sido símbolo de ligereza, renovación y la trascendencia del alma. En muchas culturas, se le atribuye la misión de unir el mundo terrenal con el espiritual, llevando mensajes de amor y esperanza más allá del tiempo. En una urna cerámica, su presencia se convierte en un emblema de eternidad: un recordatorio de que aquellos que amamos no se desvanecen, sino que permanecen en la brisa, en el canto de la naturaleza y en cada instante de belleza que nos rodea. Como el colibrí que desafía el viento con su ímpetu, el alma que habita en su refugio no se apaga, sino que sigue vibrando en el recuerdo y el amor de quienes la honran. Esta urna es más que un receptáculo; es un santuario de memoria, un homenaje delicado a la vida que continúa su vuelo, sutil y eterno, en los corazones de quienes siguen su estela.",
+    imagen: ["/images/LuzPlateada-1700.png"],
+    capacidad: 1.750,
+    gramos: "850 - 950",
+    tipo: "prm"
+  },
 ];
